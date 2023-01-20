932748f73b2f81f11651040897f3853d4b731edf
Jordan Wade <jjwade31@gmail.com>
2023-01-20 15:00:07 -0600

    Add loader component

---
+++ src/ui-kit/Loader/Loader.styles.js
@@ -0,0 +1,34 @@
+import { withTheme, css } from 'styled-components';
+import styled from 'styled-components';
+import { themeGet } from '@styled-system/theme-get';
+
+import { system } from '../_lib/system';
+
+const center = ({ centered }) => {
+  if (centered) {
+    return css`
+      justify-content: center;
+    `;
+  }
+  return null;
+};
+
+const firstChildRule = () => {
+  return css`
+    > *:first-child {
+      margin-right: ${themeGet('space.s')};
+    }
+  `;
+};
+
+const Loader = withTheme(styled.div`
+  align-items: center;
+  display: flex;
+  color: ${themeGet('colors.text.primary')};
+
+  ${firstChildRule}
+  ${center}
+  ${system}
+`);
+
+export default Loader;
