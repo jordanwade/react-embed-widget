b5521e8ab5132708686cab6682f47229dca9555d
Jordan Wade <jjwade31@gmail.com>
2023-01-23 16:14:24 -0600

    Add HorizontalCardListFeature

---
+++ src/components/FeatureFeed/Features/HorizontalCardListFeature.js
@@ -0,0 +1,33 @@
+// import { useNavigation } from '../../router';
+// import { useBreakpoint } from '../../providers/BreakpointProvider';
+
+// import { getURLFromType } from '../../utils';
+import { Box, H3, systemPropTypes } from '../../../ui-kit';
+import { apollosPropTypes } from '../../../config';
+
+function HorizontalCardListFeature(props = {}) {
+  //   const router = useNavigation();
+  //   const { responsive } = useBreakpoint();
+
+  //   const handleActionPress = (action) => {
+  //     router.push(getURLFromType(action.relatedNode));
+  //   };
+
+  return (
+    <Box pb="l" backgroundColor="#323233" {...props}>
+      <H3 mb="xs">{props.feature.title}</H3>
+      {props.feature?.cards?.map((item, index) => (
+        <Box key={index} width="200px" mr="16px">
+          <img src={item.coverImage?.sources[0]?.uri} width="100%" />
+        </Box>
+      ))}
+    </Box>
+  );
+}
+
+HorizontalCardListFeature.propTypes = {
+  ...systemPropTypes,
+  feature: apollosPropTypes.HorizontalCardListFeature,
+};
+
+export default HorizontalCardListFeature;
