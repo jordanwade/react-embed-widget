82bf22fbd455d22396488709ee1e33aeb2ad5f33
Jordan Wade <jjwade31@gmail.com>
2023-01-24 14:48:08 -0600

    Add router provider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { FeatureFeed, Auth } from './embeds';
 import { AppProvider } from './providers';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './error-page';
 
 // Maps a widget name to a Component to render it.
 const WidgetComponentMap = {
@@ -12,11 +14,19 @@ function App(props) {
   // Lookup the component responsible for rendering this Widget
   const WidgetComponent = WidgetComponentMap[props.type];
 
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <WidgetComponent symbol={props.symbol} church={props.church} />,
+      errorElement: <ErrorPage />,
+    },
+  ]);
+
   // Widgets require a church slug to get the correct data
   if (WidgetComponent && props.church) {
     return (
       <AppProvider>
-        <WidgetComponent symbol={props.symbol} church={props.church} />
+        <RouterProvider router={router} />
       </AppProvider>
     );
   }
