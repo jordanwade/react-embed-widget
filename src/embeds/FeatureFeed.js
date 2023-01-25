d3621f2b71b1f49f6ac0a3b41208bb41882e2fcd
Jordan Wade <jjwade31@gmail.com>
2023-01-25 15:43:08 -0600

    Add component routing based on searchParams to FeatureFeed

--- src/embeds/FeatureFeed.js
+++ src/embeds/FeatureFeed.js
@@ -1,19 +1,49 @@
 import React from 'react';
-import { TabFeedProvider } from '../providers';
-import { Feed } from '../components';
+import { TabFeedProvider, ContentItemProvider } from '../providers';
+import { Feed, ContentSingle } from '../components';
+import { useSearchParams } from 'react-router-dom';
 
 const FeatureFeed = (props) => {
-  return (
-    <TabFeedProvider
-      Component={Feed}
-      options={{
-        variables: {
-          tab: 'TV',
-        },
-      }}
-      {...props}
-    />
-  );
+  const [searchParams] = useSearchParams();
+  const _id = searchParams.get('id');
+
+  const [type, randomId] = _id?.split('-');
+
+  switch (type) {
+    case 'EventContentItem':
+    case 'InformationalContentItem':
+    case 'MediaContentItem':
+    case 'WeekendContentItem':
+    case 'UniversalContentItem': {
+      const options = {
+        variables: { id: `${type}:${randomId}` },
+      };
+
+      return (
+        <ContentItemProvider Component={ContentSingle} options={options} />
+      );
+    }
+    case 'ContentChannel': {
+      return <h1>Content Channel Component</h1>;
+    }
+    case 'Url': {
+      return <h1>External Url</h1>;
+    }
+    case 'FeatureFeed':
+    default: {
+      return (
+        <TabFeedProvider
+          Component={Feed}
+          options={{
+            variables: {
+              tab: 'TV',
+            },
+          }}
+          {...props}
+        />
+      );
+    }
+  }
 };
 
 FeatureFeed.propTypes = {};
