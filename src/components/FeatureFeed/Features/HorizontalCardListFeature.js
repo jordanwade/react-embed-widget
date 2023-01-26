03c1cdbcbb264032376e18b59a74bb6cb41d33a9
Jordan Wade <jjwade31@gmail.com>
2023-01-26 15:49:34 -0600

    Update horizontalCardListFeature to navigate

--- src/components/FeatureFeed/Features/HorizontalCardListFeature.js
+++ src/components/FeatureFeed/Features/HorizontalCardListFeature.js
@@ -1,26 +1,33 @@
 import React from 'react';
-// import { useNavigation } from '../../router';
-// import { useBreakpoint } from '../../providers/BreakpointProvider';
+import { useNavigate } from 'react-router-dom';
 
-// import { getURLFromType } from '../../utils';
-import { Box, H3, systemPropTypes } from '../../../ui-kit';
+import { getURLFromType } from '../../../utils';
+import { ContentCard, Box, H3, systemPropTypes } from '../../../ui-kit';
 
 function HorizontalCardListFeature(props = {}) {
-  //   const router = useNavigation();
-  //   const { responsive } = useBreakpoint();
+  const navigate = useNavigate();
 
-  //   const handleActionPress = (action) => {
-  //     router.push(getURLFromType(action.relatedNode));
-  //   };
+  const handleActionPress = (item) => {
+    navigate({
+      pathname: '/',
+      search: `?id=${getURLFromType(item.relatedNode)}`,
+    });
+  };
 
   return (
-    <Box pb="l" backgroundColor="#323233" {...props}>
+    <Box pb="l" {...props}>
       <H3 mb="xs">{props.feature.title}</H3>
-      {props.feature?.cards?.map((item, index) => (
-        <Box key={index} width="200px" mr="16px">
-          <img src={item.coverImage?.sources[0]?.uri} width="100%" />
-        </Box>
-      ))}
+      <Box display="grid" gridTemplateColumns="repeat(3, 1fr)" gridGap="20px">
+        {props.feature?.cards?.map((item, index) => (
+          <ContentCard
+            key={item.title}
+            image={item.coverImage}
+            title={item.title}
+            summary={item.summary}
+            onClick={() => handleActionPress(item)}
+          />
+        ))}
+      </Box>
     </Box>
   );
 }
