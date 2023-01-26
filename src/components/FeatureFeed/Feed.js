ad79d26b884583f614977ff8b5980d1849ecfc44
Jordan Wade <jjwade31@gmail.com>
2023-01-26 14:08:53 -0600

    Remove lodash from Feed

--- src/components/FeatureFeed/Feed.js
+++ src/components/FeatureFeed/Feed.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import isNil from 'lodash';
 
 import { Box, Loader } from '../../ui-kit';
 
@@ -54,7 +53,7 @@ const Feed = (props) => {
 
   const features = props.data?.features;
   console.log(props);
-  const renderedFeatures = features?.map(renderFeature).filter(isNil);
+  const renderedFeatures = features?.map(renderFeature);
   console.log(renderedFeatures);
   if (props.error || !renderedFeatures || !renderedFeatures.length) {
     return (
